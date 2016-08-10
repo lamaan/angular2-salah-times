b80872abd58dc8100111e3e732c021167e2fdb5a
Lamaan <lamaan@lamaan.com>
2016-08-10 06:37:26 +0300

    adding qur'an quotes to standards

--- src/app/standards/standards.component.ts
+++ src/app/standards/standards.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {TermComponent} from '../term/term.component'
+import {QuranReferenceComponent} from '../quran-reference/quran-reference.component'
 
 @Component({
   selector: 'app-standards',
   templateUrl: 'standards.component.html',
   styleUrls: ['standards.component.css'],
-  directives:[TermComponent]
+  directives:[TermComponent,QuranReferenceComponent]
 })
 export class StandardsComponent implements OnInit {
 
